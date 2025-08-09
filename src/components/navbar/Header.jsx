4ff37a19e1b7c4814d38e7f967a1a7c64da9af42
Sifa19 <naikprachita123@gmail.com>
2025-08-10 00:25:40 +0530

    constants file

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 import auth from "../../config/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { LOGO_URL } from "../../utils/constants";
 
 function Header() {
   const user = useSelector((state) => state.user);
@@ -40,11 +41,7 @@ function Header() {
     bg-gradient-to-b from-black
     flex items-center-safe"
     >
-      <img
-        className="w-28 sm:w-44"
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-24/consent/87b6a5c0-0104-4e96-a291-092c11350111/019808e2-d1e7-7c0f-ad43-c485b7d9a221/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="logo"
-      />
+      <img className="w-28 sm:w-44" src={LOGO_URL} alt="logo" />
       <div
         className="ml-auto flex items-center gap-4 cursor-pointer
       text-gray-900 text-3xl"
